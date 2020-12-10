1435ee209666e77df4235f5d53605c333868568e
William Wei <william@teaches.cc>
2020-12-11 01:39:38 +0800

    done

--- src/app/nric/nric.component.ts
+++ src/app/nric/nric.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from "@angular/core";
-import { DataService } from "../data.service";
-import { ActivatedRoute } from "@angular/router";
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 @Component({
-  selector: "app-nric",
-  templateUrl: "./nric.component.html",
-  styleUrls: ["./nric.component.scss"]
+  selector: 'app-nric',
+  templateUrl: './nric.component.html',
+  styleUrls: ['./nric.component.scss']
 })
 export class NricComponent implements OnInit {
   public nricCode;
@@ -21,31 +21,34 @@ export class NricComponent implements OnInit {
     this.$data.currentPage.subscribe(page => (this.page = page));
     this.$data.authToken.subscribe(auth => (this.auth = auth));
     this.headerData$ = this.$data.getStatusSettings();
-    this.paymentCode = this.$route.snapshot.paramMap.get("paymentCode");
+    this.paymentCode = this.$route.snapshot.paramMap.get('paymentCode');
     this.$route.queryParams.subscribe(params => {
-      this.statusModule = params["module"];
+      this.statusModule = params['module'];
     });
   }
   postpaymentCodeValidation($event) {
     ($event.target as HTMLButtonElement).disabled = true;
     if (!!this.nricCode) {
+      this.$data.pageIsLoad(false);
       this.$data
         .paymentCodeValidation(this.nricCode, this.paymentCode)
         .subscribe(res => {
           ($event.target as HTMLButtonElement).disabled = false;
-          if (res["isSuccess"]) {
-            this.$data.setAuth(res["token"]);
+          if (res['isSuccess']) {
+            this.$data.setAuth(res['token']);
             this.sendOTP();
-            this.$data.changePage("1");
+            this.$data.pageIsLoad(true);
+            this.$data.changePage('1');
           } else {
-            this.errorMsg = res["errorMessage"];
+            this.errorMsg = res['errorMessage'];
             this.error = true;
+            this.$data.pageIsLoad(true);
             ($event.target as HTMLButtonElement).disabled = false;
           }
         });
     } else {
       this.error = true;
-      this.errorMsg = "NRIC is required";
+      this.errorMsg = 'NRIC is required';
       ($event.target as HTMLButtonElement).disabled = false;
     }
   }
--- src/app/payment-status/payment-status.component.ts
+++ src/app/payment-status/payment-status.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from "@angular/core";
-import { DataService } from "../data.service";
-import { Observable } from "rxjs";
-import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: "app-payment-status",
-  templateUrl: "./payment-status.component.html",
-  styleUrls: ["./payment-status.component.css"]
+  selector: 'app-payment-status',
+  templateUrl: './payment-status.component.html',
+  styleUrls: ['./payment-status.component.css']
 })
 export class PaymentStatusComponent implements OnInit {
   public page: string;
   public headerData$: Observable<any>;
   public paymentCode;
+  public loaded: boolean;
   constructor(private $data: DataService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.paymentCode = this.route.snapshot.paramMap.get("paymentCode");
+    this.paymentCode = this.route.snapshot.paramMap.get('paymentCode');
     this.$data.currentPage.subscribe(page => (this.page = page));
+    this.$data.loading.subscribe(load => (this.loaded = load));
+    this.$data.pageIsLoad(true);
   }
 }
